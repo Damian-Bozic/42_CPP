f2cf438acccb6290e72ca5b4ceba5315f88f1dc9
Damian Bozic <dbozic@c1r3s4.42warsaw.pl>
2026-02-24 17:12:35 +0100

    Final push before splitting

--- cpp_module_09/ex02/srcs/main.cpp
+++ cpp_module_09/ex02/srcs/main.cpp
@@ -11,15 +11,36 @@
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+#include <algorithm>
 
 int main(int argc, char **argv)
 {
 	(void) argc;
 	(void) argv;
+	struct timeval start;
+	struct timeval stop;
+
 	PmergeMe sorter(TESTSTR);
-	std::cout << sorter << std::endl;
+	std::cout << "Unsorted: " << sorter << std::endl;
+	gettimeofday(&start, NULL);
 	sorter.DividePairs();
-	std::cout << sorter << std::endl;
 	sorter.initAndInsert();
-	std::cout << sorter << std::endl;
+	gettimeofday(&stop, NULL);
+	std::cout << "Sorted: " << sorter << std::endl;
+	std::cout << "Time taken for PmergeMe in microseconds: " << stop.tv_usec - start.tv_usec << std::endl;
+	
+	std::vector<int> vect;
+	std::string sequence = TESTSTR;
+
+	for (size_t i = 0; i < sequence.size(); i++) {
+		vect.push_back(atoi(sequence.substr(i, sequence.find_first_of(' ', i)).c_str()));
+		while (sequence[i] && sequence.at(i) != ' ')
+			i++;
+	}
+	std::cout << "Unsorted: " << vect << std::endl;
+	gettimeofday(&start, NULL);
+	std::sort(vect.begin(), vect.end());
+	gettimeofday(&stop, NULL);
+	std::cout << "Sorted: " << vect << std::endl;
+	std:: cout << "Time taken for vector microseconds: " << stop.tv_usec - start.tv_usec << std::endl;
 }
